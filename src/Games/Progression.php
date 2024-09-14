d211263f65a4a1230f98faf8dfc95b34825b6a43
Kreoin <steamuser85@yandex.ru>
2024-09-14 18:21:27 +0300

    create progression game

---
+++ src/Games/Progression.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace BrainGames\Progression;
+
+use function cli\line;
+use function cli\prompt;
+use function BrainGames\Engine\check;
+
+function run($name): bool
+{
+    line("What number is missing in the progression?");
+    $questionCount = 3;
+
+    for ($i = 0; $i < $questionCount; $i++) {
+        $array = generateArray();
+        $deletedIndex = rand(0, count($array) - 1);
+        $correctAnswer = (string)$array[$deletedIndex];
+        $array[$deletedIndex] = '..';
+        $question = implode(' ', $array);
+        $check = check($question, $correctAnswer);
+
+        if (!$check) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function generateArray(): array
+{
+    $step = rand(1, 10);
+    $start = rand(1, 100);
+    $size = rand(5, 10);
+    $end = $start + $size * $step;
+
+    return range($start, $end, $step);
+}
